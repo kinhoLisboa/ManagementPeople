fa095ef6e86decb60f594383b15cdaf5fec79f5a
Danifleikson Buriti Lisboa <danifleikson@hotmail.com>
2023-09-27 18:33:39 -0300

    Inicialisando a aplicação !

---
+++ src/main/java/com/gerenciamento/pessoas/ManagementPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciamento/pessoas/ManagementPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciamento.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
